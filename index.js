b52a2add0a0119f7c15c0ef506efc0af15e3a43a
rishabh491 <rishabhsahani491@gmail.com>
2025-06-21 19:30:51 +0530

    ....initialsetup

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express'); 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express Backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
